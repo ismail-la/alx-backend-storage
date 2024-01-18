262d1f57d15b3f8269ac5256a4c1abf1ea0c3f85
ismail-la <lahbariismail@gmail.com>
2024-01-18 21:45:12 +0100

    0x00. MySQL advanced

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a table users with id, email, name
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
