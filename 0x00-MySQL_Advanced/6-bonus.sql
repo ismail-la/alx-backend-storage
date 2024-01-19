1837e2c0dc47422a51138744ea22b45b0ba30571
ismail-la <lahbariismail@gmail.com>
2024-01-19 03:21:33 +0100

    0x00. MySQL advanced

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,12 +1,12 @@
 -- SQL script creates a stored procedure named 'AddBonus' which takes in three parameters user_id, project_name and score.
 
 DELIMITER $$;
-CREATE PROCEDURE AddBonus(IN a_user_id INT, IN a_project_name VARCHAR(255), IN a_score INT )
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
 BEGIN
-	IF NOT EXISTS(SELECT name FROM projects WHERE name=a_project_name) THEN
-		INSERT INTO projects (name) VALUES (a_project_name);
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
-	INSERT INTO corrections (a_user_id, a_project_id, a_score)
-	VALUES (a_user_id, (SELECT id from projects WHERE name=a_project_name), a_score);
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
 END;$$
 DELIMITER ;
